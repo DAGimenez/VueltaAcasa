620907f2c98e06577237e60e2e7538afd24fe7cd
Santy834 <santiago21.benitez@gmail.com>
2026-05-23 17:35:30 -0300

    Sistema de estamina V1

--- Player1.gd
+++ Player1.gd
@@ -1,23 +1,59 @@
 extends CharacterBody3D
 
+@onready var vista : Node3D = $vista
+@onready var camera_3d: Camera3D = $vista/Camera3D
 
-@export var SPEED := 0.0
-@export var JUMP_VELOCITY := 0.0
+@export var mouse_sensitivity : float = 0.005
+@export var SPEED : float = 0.0
+@export var SPRINT : float = 10.0
+@export var JUMP_VELOCITY : float = 0.0
+
+var rotation_x := 0.0
+var sprinting : bool = false
+var stamina : float = 100.0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		
+		rotate_y(-event.relative.x * mouse_sensitivity)
+
+		# Rotación vertical
+		rotation_x -= event.relative.y * mouse_sensitivity
+
+		# Limitar mirada arriba/abajo
+		rotation_x = clamp(rotation_x, deg_to_rad(-90), deg_to_rad(90))
+
+		vista.rotation.x = rotation_x
 
 func _ready() -> void:
-	SPEED = 8.0
+	SPEED = 6.0
 	JUMP_VELOCITY = 6.0
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta : float) -> void:
 	_salto(delta)
-	_movimiento()
+	_movimiento(delta)
 
-func _movimiento():
+func _movimiento(delta : float):
+	if Input.is_action_pressed("Sprint") and stamina > 0.0:
+		sprinting = true
+		if stamina <= 0:
+			stamina -= stamina * delta
+		
+	else:
+		sprinting = false
+		if stamina <= 100:
+			stamina += stamina * delta
+		
+	
+	print("Mi estamina es: ", stamina)
 
 	var input_dir := Input.get_vector("A", "D", "W", "S")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
-	if direction:
+	if direction and sprinting:
+		velocity.x = direction.x * SPRINT
+		velocity.z = direction.z * SPRINT
+	elif direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
 	else:
