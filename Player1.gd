9d5e6931df0790e19ad42826d90e9ae4858cccca
Gorositop15 <gorositopablocabj@gmail.com>
2026-07-10 14:23:51 -0300

    pj primera persona se agacha
    codigo para que el pj en primera persona se agache

--- Player1.gd
+++ Player1.gd
@@ -7,6 +7,7 @@ extends CharacterBody3D
 @export var SPEED : float = 3.0
 @export var SPRINT : float = 10.0
 @export var JUMP_VELOCITY : float = 0.0
+@export var CROUCH_SPEED : float = 1.75
 
 
 var rotation_x := 0.0
@@ -15,6 +16,9 @@ var stamina : float = 200.0
 var MIN_SPRINT := 175.0
 var recovery := 100.0
 var decrement := 50.0
+var altura_parada :float = 2.0
+var altura_agachada :float = 0.5
+var esta_agachada : bool = false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
@@ -33,10 +37,13 @@ func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta : float) -> void:
+	agacharse()
 	_salto(delta)
 	_movimiento(delta)
 	actualizar_stamina()
 
+
+			
 func actualizar_stamina():
 	if stamina >= 133.34:
 		SPRINT = 8.0
@@ -59,15 +66,20 @@ func _movimiento(delta : float):
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if direction:
-		if sprinting:
+		if sprinting and !esta_agachada:
 			velocity.x = direction.x * SPRINT
 			velocity.z = direction.z * SPRINT
 			stamina -= recovery * delta
 			stamina = clamp(stamina, 0, 200)
-		else:
+		elif !esta_agachada:
 			stamina += decrement * delta
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
+			stamina = clamp(stamina, 0, 200)
+		elif esta_agachada: 		#cuando esté agachada la stamina sigue creciendo pero su velocidad disminuye
+			stamina += decrement * delta
+			velocity.x = direction.x * CROUCH_SPEED
+			velocity.z = direction.z * CROUCH_SPEED 
 			stamina = clamp(stamina, 0, 200)
 	else:
 		stamina += decrement * delta
@@ -76,7 +88,14 @@ func _movimiento(delta : float):
 		stamina = clamp(stamina, 0, 200)
 	
 	move_and_slide()
-
+	
+func agacharse():
+	if Input.is_action_just_pressed("agacharse"):
+		esta_agachada = !esta_agachada
+	if esta_agachada:
+		$CollisionShape3D.shape.height = altura_agachada
+	else:
+		$CollisionShape3D.shape.height = altura_parada
 
 func _salto(delta : float):
 		# Add the gravity.
